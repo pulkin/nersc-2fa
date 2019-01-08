f4906c18daebbd505275fb02cecc61f05cefc023
Artem <gpulkin@gmail.com>
2019-01-08 16:58:59 +0100

    Meat

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 nersc-2fa
+=========
+
+A seamless way to get a temporary ssh certificate for NERSC machines.
+
+Installation
+------------
+
+1. Install dependencies
+
+```
+pip install pyotp
+```
+
+2. Clone this repo
+
+```
+git clone https://github.com/pulkin/nersc-2fa.git
+```
+
+3. Run the script
+
+```
+./nersc-2fa.py
+```
+
+Then, enter your login, password, and the generated token seed.
+
+**Warning!** All your login credentials will be stored plain-text in `.config/nersc-2fa.json` file. It is your responsibility to keep this file safe.
+
+Usage
+-----
+```
+./nersc-2fa.py
+```
+The script checks the certificate and updates it if needed.
---
+++ nersc-2fa.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+import pyotp
+import base64
+import requests
+import os
+import json
+import getpass
+
+import subprocess
+import tempfile
+from datetime import datetime
+
+config_file = os.path.expanduser("~/.config/nersc-2fa.json")
+
+def load_config(fname):
+    with open(fname, 'r') as f:
+        config = json.load(f)
+    return config["user"], base64.b64decode(config["password"]), config["url"], config["seed"], config["target"]
+
+def save_config(fname, user, password, url, seed, target):
+    with open(fname, 'w') as f:
+        json.dump(dict(user=user, password=base64.b64encode(password), url=url, seed=seed, target=target), f, indent=2)
+
+try:
+    user, password, url, seed, target = load_config(config_file)
+    print("Configuration read from {}".format(config_file))
+
+except Exception as e:
+
+    # === User === #
+    default_user = getpass.getuser()
+    user = raw_input("User (default: {}): ".format(default_user))
+    if user == "":
+        user = default_user
+
+    # === Password === #
+    password = getpass.getpass()
+    config["password"] = password
+
+    # === Seed === #
+    while True:
+        seed = raw_input("Token seed: ")
+        try:
+             pyotp.TOTP(seed).now()
+        except:
+            print("This is not a valid token.")
+        else:
+            break
+
+    # === Target === #
+    default_target = "nersc"
+    target = raw_input("Target file name (default: {}): ".format(default_target))
+    if target == "":
+        target = default_target
+
+    # === URL === #
+    default_url = 'https://sshproxy.nersc.gov/create_pair/default/'
+    while True:
+        url = raw_input("Target URL (default: {}): ".format(default_url))
+        if url == "":
+            url = default_url
+        try:
+            if requests.post(url).status_code == 401:
+                break
+            else:
+                print("Not a valid URL: does not return 401")
+        except:
+            print("Not a valid URL: exception occurs")
+
+    save_config(config_file, user, password, url, seed, target)
+    print("Saved configuration to {}".format(config_file))
+
+def get_cert(user, password, seed):
+    r = requests.post(url, auth=(user, password + pyotp.TOTP(seed).now()))
+    if r.status_code == 401:
+        raise RuntimeError("Failed to authenticate. Server response:\n{0.text}".format(r))
+    elif r.status_code == 200:
+        return r.text
+    else:
+        raise RuntimeError("HTTP error {0.status_code:d} {0.reason}. Server response:\n{0.text}".format(r))
+
+def deploy_cert(cert, location):
+    with open(location, 'w') as f:
+        f.write(cert)
+    os.chmod(location, 0600)
+
+def is_cert_valid(location):
+    if not os.path.isfile(location):
+        return False
+    with open(location, 'r') as f:
+        text = f.read()
+        if "ssh-rsa" not in text:
+            return False
+        text = text[text.find("ssh-rsa"):]
+    tfile = tempfile.NamedTemporaryFile()
+    tfile.write(text)
+    tfile.flush()
+    output = subprocess.check_output(["ssh-keygen", "-L", "-f", tfile.name])
+    if "Valid" not in output:
+        return False
+    output = output[output.find("Valid"):]
+    output = output[output.find("to")+3:output.find("\n")]
+    t = datetime.strptime(output, "%Y-%m-%dT%H:%M:%S")
+    return t > datetime.now()
+
+cert_location = os.path.expanduser("~/.ssh/" + target)
+
+if not is_cert_valid(cert_location):
+    print("Updating certificate ...")
+    deploy_cert(get_cert(user, password, seed), cert_location)
+else:
+    print("The certificate is still valid, nothing to do")
